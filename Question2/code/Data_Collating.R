8774c9f11083dc386b9f9dc2b6b0aa78a486b87a
Jeanne9 <jeannebrink9@gmail.com>
2021-05-24 20:19:57 +0200

    Question 2

---
+++ Question2/code/Data_Collating.R
@@ -0,0 +1,23 @@
+# Collating data
+Data_Collating <- function(Datroot = "Question2/data/"){
+  
+  library(tidyverse)
+  # first create a silent read function
+  silentread <- function(x){
+    hushread <- purrr::quietly(read_csv)
+    df <- hushread(x)
+    df$result
+  }
+  
+  Forbesdata <-
+    list.files(Datroot, full.names = T, recursive = T) %>% 
+    mutate(Age = as.character(Age)) %>%
+    #Only load csv's, not readme's. 
+    .[!grepl(".txt", .)] %>%
+    as.list() %>% 
+    map(~silentread(.)) %>% 
+    bind_rows()
+  
+  Forbesdata
+  
+}
